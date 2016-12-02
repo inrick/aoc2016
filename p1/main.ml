3801c07dd701280be552ee52bcc21181daf79a9d
Fabian Carlström <inrick@gmail.com>
2016-12-02 17:09:28 +0100

    Cleanup

--- p1/main.ml
+++ p1/main.ml
@@ -22,22 +22,23 @@ let directions lexbuf =
 
 let parse s = Lexing.from_string s |> directions
 
-let navigate start_orient start_coord dirs =
-  let move orient dir = match orient, dir with
+let move current_pos orient dir =
+  let new_orient, diff = match orient, dir with
   | N, L i | S, R i -> W, (-i, 0)
   | N, R i | S, L i -> E, (i, 0)
   | W, L i | E, R i -> S, (0, -i)
   | W, R i | E, L i -> N, (0, i) in
-  let _, end_coord = List.fold_left dirs ~init:(start_orient, start_coord) ~f:(
-    fun (orient, coord) dir ->
-      let new_orient, coord_diff = move orient dir in
-      new_orient, Coord.(coord + coord_diff)) in
-  end_coord
+  new_orient, Coord.(current_pos + diff)
+
+let navigate start_orient start_pos dirs =
+  let _, end_pos = List.fold_left dirs ~init:(start_orient, start_pos) ~f:(
+    fun (orient, pos) dir -> move pos orient dir) in
+  end_pos
 
 let () =
   let dirs = In_channel.read_all "input.txt" |> parse in
-  let start_coord = 0, 0 in
-  let end_coord = navigate N start_coord dirs in
-  let x, y = end_coord in
+  let start_pos = 0, 0 in
+  let end_pos = navigate N start_pos dirs in
+  let x, y = end_pos in
   printf "End coordinate: (%d, %d), distance: %d\n" x y
-    (Coord.dist start_coord end_coord)
+    (Coord.dist start_pos end_pos)
