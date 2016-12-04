447858ca9fb0671b9ed174917d3da13e7c4252cc
Fabian Carlström <inrick@gmail.com>
2016-12-04 15:19:33 +0100

    Factor out solver to only depend on KEYPAD interface
    To prepare for the other implementation of keypad needed for part 2.

--- p2/main.ml
+++ p2/main.ml
@@ -1,9 +1,18 @@
 open Core_kernel.Std
 
-module Keypad = struct
+type instr = U | D | L | R
+
+module type KEYPAD = sig
+  type state
+
+  val initial_state : state
+  val move : state -> instr -> state
+  val key : state -> string
+end
+
+module Keypad1 = struct
   type t = int array array
   type state = int * int
-  type instr = U | D | L | R
 
   let keypad = [|[|1;2;3|];[|4;5;6|];[|7;8;9|]|]
   let initial_state = 1, 1 (* 5 on keypad *)
@@ -16,12 +25,20 @@ module Keypad = struct
     | L -> row, try_move (col-1)
     | R -> row, try_move (col+1)
 
-  let key (x, y) = keypad.(x).(y)
+  let key (x, y) = string_of_int keypad.(x).(y)
+end
+
+module Solver(K : KEYPAD) = struct
+  let solve input =
+    List.fold_left input ~init:(K.initial_state, []) ~f:(
+      fun (state, code) instrs ->
+        let end_state = List.fold_left instrs ~init:state ~f:K.move in
+        end_state, (end_state::code))
+    |> snd |> List.rev_map ~f:K.key |> String.concat
 end
 
 let lex_instrs lexbuf =
   let open Lexer in
-  let open Keypad in
   List.map (tokens lexbuf) (function
     | UP -> U | DOWN -> D | LEFT -> L | RIGHT -> R | EOF -> assert false)
 
@@ -29,10 +46,6 @@ let parse s = Lexing.from_string s |> lex_instrs
 
 let () =
   let input = In_channel.read_lines "input.txt" |> List.map ~f:parse in
-  let code =
-    List.fold_left input ~init:(Keypad.initial_state, []) ~f:(
-      fun (state, code) instrs ->
-        let end_state = List.fold_left instrs ~init:state ~f:Keypad.move in
-        end_state, (end_state::code))
-    |> snd |> List.rev_map ~f:Keypad.key in
-  List.map code string_of_int |> String.concat |> print_endline
+  let module S1 = Solver(Keypad1) in
+  let code1 = S1.solve input in
+  print_endline code1
