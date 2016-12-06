cdccd639711a2263d34730c42e320538bdaed0c0
Fabian Carlström <inrick@gmail.com>
2016-12-06 10:26:32 +0100

    Solve part 1 of day 1 in numpy

---
+++ p1/main.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+input = open('input.txt','r').readlines()[0].rstrip().split(', ')
+directions = np.array([1 if x == 'R' else -1 for x in (x[0] for x in input)])
+magnitudes = np.array([int(x[1:]) for x in input])
+moves = np.array([[0,1],[1,0],[0,-1],[-1,0]])[np.mod(np.cumsum(directions), 4)]
+print(sum(abs(magnitudes @ moves))) # part 1
