4979c4736fa70368936ee183c864abbdf47078db
Fabian Carlström <inrick@gmail.com>
2016-12-06 20:52:13 +0100

    Hack together special transpose3 function
    That will crash on unexpected input. Since we only need this special
    case.

--- p3/main.ml
+++ p3/main.ml
@@ -15,10 +15,15 @@ let groupby3 lst =
     | _ -> assert false in
   go [] lst
 
+let transpose3 = function (* List.transpose_exn already does this in general *)
+  | [[x1;y1;z1];[x2;y2;z2];[x3;y3;z3]] ->
+      [[x1;x2;x3];[y1;y2;y3];[z1;z2;z3]]
+  | _ -> assert false
+
 let () =
   let input = In_channel.read_all "input.txt" |> parse in
   let possible = List.(filter input is_possible |> length) in
   printf "Part 1, possible: %d\n" possible;
-  let input = List.(groupby3 input >>| transpose_exn |> concat) in
+  let input = List.(groupby3 input >>| transpose3 |> concat) in
   let possible = List.(filter input is_possible |> length) in
   printf "Part 2, possible: %d\n" possible;
