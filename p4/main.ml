a5d6545a96ceaa64f94d842118550709649571da
Fabian Carlström <inrick@gmail.com>
2016-12-08 21:59:01 +0100

    Solve part 2 of day 4

--- p4/main.ml
+++ p4/main.ml
@@ -5,7 +5,7 @@ type room = {name : string; id: int; chk : string}
 
 let parse s =
   let open String in
-  let name = slice s 0 (-10) |> filter ~f:(Char.(<>) '-') in
+  let name = slice s 0 (-11) in
   let id = slice s (-10) (-7) |> int_of_string in
   let chk = slice s (-6) (-1) in
   {name; id; chk}
@@ -13,7 +13,7 @@ let parse s =
 let validate r =
   let open List in
   let chk =
-    String.to_list r.name
+    String.(filter r.name (Char.(<>) '-') |> to_list)
     |> sort ~cmp:Char.compare
     |> group ~break:(<>)
     |> sort ~cmp:(fun xs ys -> length ys - length xs)
@@ -22,10 +22,21 @@ let validate r =
     |> String.of_char_list in
   chk = r.chk
 
+let alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+let rotate n c =
+  if c = '-' then ' '
+  else alphabet.[(String.index_exn alphabet c + n) mod 26]
+
+let decrypt r = {r with name = String.map r.name (rotate r.id)}
+
 let () =
   let input = In_channel.read_lines "input.txt" in
   let rooms = List.map input parse in
   let sum =
     List.filter_map rooms ~f:(fun r -> if validate r then Some r.id else None)
     |> List.fold_left ~init:0 ~f:(+) in
-  printf "%d\n" sum
+  printf "%d\n" sum;
+  let candidate = List.(rooms >>| decrypt |> find_exn ~f:(fun r ->
+    String.substr_index r.name ~pattern:"north" |> Option.is_some)) in
+  printf "%d\n" candidate.id
