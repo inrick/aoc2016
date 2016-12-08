d23078d174670857ee9301825fee3bc636de9213
Fabian Carlström <inrick@gmail.com>
2016-12-08 22:07:21 +0100

    Use List.groupi in day 3 solution

--- p3/main.ml
+++ p3/main.ml
@@ -8,13 +8,6 @@ let is_possible lst = match List.sort ~cmp:compare lst with
   | [x;y;z] -> x+y > z
   | _ -> assert false
 
-let groupby3 lst =
-  let rec go acc = function
-    | x :: y :: z :: tl -> go ([x;y;z]::acc) tl
-    | [] -> List.rev acc
-    | _ -> assert false in
-  go [] lst
-
 let transpose3 = function (* List.transpose_exn already does this in general *)
   | [[x1;y1;z1];[x2;y2;z2];[x3;y3;z3]] ->
       [[x1;x2;x3];[y1;y2;y3];[z1;z2;z3]]
@@ -24,6 +17,7 @@ let () =
   let input = In_channel.read_all "input.txt" |> parse in
   let possible = List.(filter input is_possible |> length) in
   printf "Part 1, possible: %d\n" possible;
-  let input = List.(groupby3 input >>| transpose3 |> concat) in
+  let input =
+    List.(groupi input (fun i _ _ -> i mod 3 = 0) >>| transpose3 |> concat) in
   let possible = List.(filter input is_possible |> length) in
   printf "Part 2, possible: %d\n" possible;
