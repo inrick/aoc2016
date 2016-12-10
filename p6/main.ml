bd2b60393134bfaca8c03236c047288d6f8c32c8
Fabian Carlström <inrick@gmail.com>
2016-12-10 13:27:34 +0100

    Factor out common code in day 6

--- p6/main.ml
+++ p6/main.ml
@@ -1,26 +1,21 @@
 open Core_kernel.Std
 
-let solve xs =
+let solve xs solver =
   let open List in
   xs >>| String.to_list
     |> transpose_exn
-    >>| (fun ys ->
-      sort ~cmp:(fun a b -> count ys ((=) b) - count ys ((=) a)) ys)
+    >>| solver
     >>| hd
     >>= Option.to_list
     |> String.of_char_list
 
-let solve2 xs =
-  let open List in
-  xs >>| String.to_list
-    |> transpose_exn
-    >>| (fun ys ->
-      sort ~cmp:(fun a b -> count ys ((=) a) - count ys ((=) b)) ys)
-    >>| hd
-    >>= Option.to_list
-    |> String.of_char_list
+let solver1 chrs =
+  List.(sort ~cmp:(fun a b -> count chrs ((=) b) - count chrs ((=) a)) chrs)
+
+let solver2 chrs =
+  List.(sort ~cmp:(fun a b -> count chrs ((=) a) - count chrs ((=) b)) chrs)
 
 let () =
   let input = In_channel.read_lines "input.txt" in
-  solve input |> print_endline;
-  solve2 input |> print_endline;
+  solve input solver1 |> print_endline;
+  solve input solver2 |> print_endline;
