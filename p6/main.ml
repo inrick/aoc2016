df05de5e2ed9468a9c6740e520812e9169307407
Fabian Carlström <inrick@gmail.com>
2016-12-10 13:24:49 +0100

    Solve part 2 of day 6

--- p6/main.ml
+++ p6/main.ml
@@ -10,6 +10,17 @@ let solve xs =
     >>= Option.to_list
     |> String.of_char_list
 
+let solve2 xs =
+  let open List in
+  xs >>| String.to_list
+    |> transpose_exn
+    >>| (fun ys ->
+      sort ~cmp:(fun a b -> count ys ((=) a) - count ys ((=) b)) ys)
+    >>| hd
+    >>= Option.to_list
+    |> String.of_char_list
+
 let () =
   let input = In_channel.read_lines "input.txt" in
-  solve input |> print_endline
+  solve input |> print_endline;
+  solve2 input |> print_endline;
