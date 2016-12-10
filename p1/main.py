969a3ada390ed93a672833c57045b6284d4f8c26
Fabian Carlström <inrick@gmail.com>
2016-12-10 10:33:05 +0100

    Use numpy function instead of py3 operator

--- p1/main.py
+++ p1/main.py
@@ -4,4 +4,4 @@ input = open('input.txt','r').readlines()[0].rstrip().split(', ')
 directions = np.array([1 if x == 'R' else -1 for x in (x[0] for x in input)])
 magnitudes = np.array([int(x[1:]) for x in input])
 moves = np.array([[0,1],[1,0],[0,-1],[-1,0]])[np.mod(np.cumsum(directions), 4)]
-print(sum(abs(magnitudes @ moves))) # part 1
+print(sum(abs(np.dot(magnitudes, moves)))) # part 1
