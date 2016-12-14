de8a2641794fa64168b49a80e85b7bd5e536e01c
Fabian Carlström <inrick@gmail.com>
2016-12-14 21:26:39 +0100

    Solve part 2 of day 13

--- p13/main.ml
+++ p13/main.ml
@@ -9,7 +9,8 @@ let count_ones =
 let is_open (x,y) =
   x*x + 3*x + 2*x*y + y + y*y + 1364 |> count_ones |> fun a -> a land 0b1 = 0
 
-let neighs (x,y) = [x+1,y;x,y+1;x-1,y;x,y-1] |> List.filter ~f:is_open
+let neighs (x,y) = [x+1,y;x,y+1;x-1,y;x,y-1]
+  |> List.filter ~f:(fun (x,y) -> is_open (x,y) && x >= 0 && y >= 0)
 
 let find_path target =
   let dist_tbl = Hashtbl.create ~hashable:Hashtbl.Poly.hashable () in
@@ -34,6 +35,29 @@ let find_path target =
       end in
   go Fheap.(add (create ~cmp:cmp_coords) (0,0))
 
+let can_reach from steps =
+  let dist_tbl = Hashtbl.create ~hashable:Hashtbl.Poly.hashable () in
+  Hashtbl.set dist_tbl ~key:(0,0) ~data:0;
+  let add_neighs d coords  =
+    List.iter coords (fun c -> Hashtbl.set dist_tbl ~key:c ~data:d) in
+  let visited = Hashtbl.mem dist_tbl in
+  let d coord = match Hashtbl.find dist_tbl coord with
+    | Some n -> n
+    | None -> 1 lsl 31 in
+  let cmp_coords a b = d a - d b in
+  let rec go n heap =
+    match Fheap.pop heap with
+    | None -> n
+    | Some (pos, heap) ->
+      let dist = d pos in
+      if dist < steps then
+        let ns = neighs pos |> List.filter ~f:(fun c -> not (visited c)) in
+        add_neighs (dist+1) ns;
+        go (n+1) (List.fold ns ~init:heap ~f:Fheap.add)
+      else go n heap in
+  go 0 Fheap.(add (create ~cmp:cmp_coords) from)
+
 let () =
   let target = 31, 39 in
-  find_path target |> printf "%d\n"
+  find_path target |> printf "%d\n";
+  can_reach (0,0) 50 |> printf "%d\n";
