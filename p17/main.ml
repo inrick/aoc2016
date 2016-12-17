880832d4ebd28b8d2c4f3de3316aa7f137ae4a46
Fabian Carlström <inrick@gmail.com>
2016-12-17 12:16:00 +0100

    Solve part 2 of day 17

--- p17/main.ml
+++ p17/main.ml
@@ -50,9 +50,12 @@ let paths target passcode =
   go S.empty {pos=0,0; path=""}
 
 let () =
+  let open List in
   let input = "njfxhljp" in
-  let ps = paths (3,-3) input in
-  S.to_list ps
-    |> List.sort ~cmp:(fun x y -> String.(length x - length y))
-    |> List.hd_exn
-    |> print_endline
+  let ps = paths (3,-3) input |> S.to_list in
+  let path1 =
+    sort ps ~cmp:(fun x y -> String.(length x - length y)) |> hd_exn in
+  print_endline path1;
+  let path2 =
+    sort ps ~cmp:(fun x y -> String.(length y - length x)) |> hd_exn in
+  printf "%d\n" (String.length path2);
