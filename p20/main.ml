9aaadfd8ba0064ce424239e8879ff89835b47fb6
Fabian Carlström <inrick@gmail.com>
2016-12-20 23:25:05 +0100

    Solve part 2 of day 20

--- p20/main.ml
+++ p20/main.ml
@@ -10,9 +10,19 @@ let find =
     | (x,y)::xs -> go (min from x) (max until y) xs in
   go 0 0
 
+let count =
+  let rec go n from until = function
+    | [] -> n
+    | (x,y)::xs when succ until < x ->
+      go (n + (x - succ until)) from (max until y) xs
+    | (x,y)::xs -> go n (min from x) (max until y) xs in
+  go 0 0 0
+
 let () =
   let input = In_channel.read_lines "input.txt"
     |> List.map ~f:parse
     |> List.sort ~cmp:(fun (a,_) (b,_) -> compare a b) in
   let smallest = find input in
   printf "%d\n" smallest;
+  let allowed = count input in
+  printf "%d\n" allowed;
