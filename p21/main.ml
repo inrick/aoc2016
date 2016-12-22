d93c872bc1409a649a26c2a7301c739bfa9bebad
Fabian Carlström <inrick@gmail.com>
2016-12-22 20:12:52 +0100

    Refactor day 21: separate instruction evaluation

--- p21/main.ml
+++ p21/main.ml
@@ -8,7 +8,6 @@ type instr =
   | Rot_rel of char
   | Reverse of int * int
   | Move of int * int
-  [@@deriving show]
 
 let parse s =
   let open Lexer in
@@ -26,51 +25,43 @@ let parse s =
     | MOVE (x, y) -> next (Move (x, y)) in
   go [] (read lexbuf)
 
-let scramble s instrs =
-  let open String in
-  let len = length s in
-  let rec go s = function
-    | [] -> s
-    | i::is ->
-      match i with
-      | Swap_pos (x, y) ->
-        let t = copy s in
-        t.[x] <- s.[y];
-        t.[y] <- s.[x];
-        go t is
-      | Swap_char (x, y) ->
-        go (map s
-          (fun c -> Char.(if c = x then y else if c = y then x else c))) is
-      | Rot_left x ->
-        go (init len (fun i -> s.[Int.((i+x) % len)])) is
-      | Rot_right x ->
-        go (init len (fun i -> s.[Int.((i-x) % len)])) is
-      | Rot_rel x ->
-        let rot = match index s x with
-          | None -> assert false
-          | Some i -> 1 + i + if Int.(i >= 4) then 1 else 0 in
-        go s (Rot_right rot::is)
-      | Reverse (x, y) ->
-        let t = copy s in
-        for i = 0 to y-x do
-          t.[x+i] <- s.[y-i]
-        done;
-        go t is
-      | Move (x, y) ->
-        let open Int in
-        let t =
-          if x < y then init len (fun i ->
-            if i = y then s.[x]
-            else if x <= i && i < y then s.[i+1]
-            else s.[i])
-          else init len (fun i ->
-            if i = y then s.[x]
-            else if y < i && i <= x then s.[i-1]
-            else s.[i]) in
-        go t is in
-  go s instrs
+let rec eval s =
+  let module B = BytesLabels in
+  let len = String.length s in
+  function
+  | Swap_pos (x, y) ->
+    let t = B.of_string s in
+    B.set t x s.[y];
+    B.set t y s.[x];
+    B.to_string t
+  | Swap_char (x, y) ->
+    String.map s (fun c -> if c = x then y else if c = y then x else c)
+  | Rot_left x -> String.init len (fun i -> s.[Int.((i+x) % len)])
+  | Rot_right x -> String.init len (fun i -> s.[Int.((i-x) % len)])
+  | Rot_rel x ->
+    let rot = match String.index s x with
+      | None -> assert false
+      | Some i -> 1 + i + if i >= 4 then 1 else 0 in
+    eval s (Rot_right rot)
+  | Reverse (x, y) ->
+    let t = B.of_string s in
+    for i = 0 to y-x do
+      B.set t (x+i) s.[y-i]
+    done;
+    B.to_string t
+  | Move (x, y) ->
+    if x < y then String.init len (fun i ->
+      if i = y then s.[x]
+      else if x <= i && i < y then s.[i+1]
+      else s.[i])
+    else String.init len (fun i ->
+      if i = y then s.[x]
+      else if y < i && i <= x then s.[i-1]
+      else s.[i])
+
+let scramble s instrs = List.fold instrs ~init:s ~f:eval
 
 let () =
-  let input = In_channel.read_all "input.txt" |> parse in
-  let scrambled = scramble "abcdefgh" input in
+  let instrs = In_channel.read_all "input.txt" |> parse in
+  let scrambled = scramble "abcdefgh" instrs in
   print_endline scrambled;
